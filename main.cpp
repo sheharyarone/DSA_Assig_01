afc1c53edcfdc2e38ab7ac1659588dcedfbce4b4
Sheharyar <sheharyarone@gmail.com>
2022-10-23 16:15:09 +0500

    main file

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@ int main()
   // mainMenu(); //call the main menu function from Parser.h
   linkedList *p=new linkedList();
   readFileData(p);
-  // writeToFile(p->head);
+  writeToFile(p->head);
   // printReverse(p->head);
+  // p->printll();
 
-  linkedList *f=reverseList(p);
-  f->printll();
+  // linkedList *f=reverseList(p);
+  // f->printll();
   
+  // linkedList *f=  filterByLetters(p,'i');
+  // f->printll();
+
   return 0;
 
 }
